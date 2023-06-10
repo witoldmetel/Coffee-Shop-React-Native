8b7e18d0790d6e091dc9fd502bf90d199ace804b
witoldmetel <witoldmetel@gmail.com>
2023-06-10 09:14:17 +0200

    web app cleanup

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,8 @@
 import { authRouter } from "./router/auth";
 import { likeRouter } from "./router/like";
-import { postRouter } from "./router/post";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-  postxd: postRouter,
   auth: authRouter,
   like: likeRouter,
 });
