73f4c431f9d76b6c6fb4df52fedce41603f2b44a
witoldmetel <witoldmetel@gmail.com>
2023-06-15 08:45:23 +0200

    update styles for product

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -10,6 +10,9 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: {
+    domains: ["firtman.github.io"],
+  },
 };
 
 export default config;
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { NextPage } from "next";
+import Image from "next/image";
 
 import { Container } from "~/components";
 import { useMenu } from "~/hooks/useMenu";
@@ -28,6 +29,9 @@ const Home: NextPage = () => {
     setSearchQuery(event.target.value);
   };
 
+  const getImage = (url: string) =>
+    `https://firtman.github.io/coffeemasters/api/images/${url}`;
+
   return (
     <Container>
       <input
@@ -37,22 +41,43 @@ const Home: NextPage = () => {
         placeholder="Search..."
         className="mb-4 rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      {menuItems?.map((category) => (
-        <div key={category.name}>
-          <div className="bg-[#ede0d4] p-4">
-            <p className="text-base font-bold text-[#410413]">
-              {category.name}
-            </p>
+      {menuItems && menuItems.length ? (
+        menuItems.map((category) => (
+          <div key={category.name}>
+            <div className="bg-[#ede0d4] p-4">
+              <p className="text-base font-bold text-[#410413]">
+                {category.name}
+              </p>
+            </div>
+            <div className="grid grid-flow-row-dense auto-rows-max grid-cols-3 gap-4 bg-white p-4">
+              {category.products.map((product) => (
+                <div key={product.id} className="rounded bg-[#ede0d4]">
+                  <div className="flex flex-col items-center">
+                    <div className="w-full">
+                      <Image
+                        src={getImage(product.image)}
+                        alt={product.name}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: "100%", height: "auto" }}
+                      />
+                    </div>
+                    <div className="flex w-full items-center justify-between p-4">
+                      <h3 className="text-lg font-medium">{product.name}</h3>
+                      <p className="text-gray-600">$ {product.price}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="grid grid-flow-row-dense auto-rows-max grid-cols-3 gap-4 bg-white p-4">
-            {category.products.map((product) => (
-              <div key={product.id} className="w-32">
-                {product.name}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p className="flex-1" color="#410413">
+          We don&apos;t have such thing in our store
+        </p>
+      )}
     </Container>
   );
 };
